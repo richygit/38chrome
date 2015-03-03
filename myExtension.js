7b6b9f7fc8ea2bfd9eee6335ff99df74d9eed4e2
richygit <richard@getup.org.au>
2015-03-03 13:39:40 +0000

    added in hide button

--- myExtension.js
+++ myExtension.js
@@ -24,7 +24,8 @@ var runCode = function() {
   }
 
   function displayHideButton() {
-    $('body').append('<div class="hide-button" style="position: fixed; bottom: 500px, right: 40px; z-index: 999;">X</div>');
+    $('body').append('<div class="hide-button" style="position: fixed; z-index: 999; bottom: 460px; right: 39px; background-color: #ddd; text-align: center; padding: 10px 15px;cursor: pointer;">X</div>');
+    $('body').append("<script>$('.hide-button').click(function() {$('.member-details').hide();});</script>");
   }
 
   gmail.observe.after('open_email', function(id, url, body) {
