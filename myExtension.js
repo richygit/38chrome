26d2270ba1eb039bd7e259e07748c567e76ab2c7
richygit <richard@getup.org.au>
2015-03-13 17:09:10 +0000

    fixed typo

--- myExtension.js
+++ myExtension.js
@@ -35,7 +35,7 @@ var runCode = function(mode) {
     $('body').append("<script>$('.hide-button').click(function() {$('.member-details').hide(); $('.hide-button').hide(); });</script>");
   }
 
-  if(model === 'gmail') {
+  if(mode === 'gmail') {
     gmail.observe.after('open_email', function(id, url, body) {
       console.log('#open email event', id);
       var emailAddr = getEmailAddr(id);
